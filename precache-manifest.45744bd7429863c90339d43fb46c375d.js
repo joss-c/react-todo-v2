933099bf8d36bb83f54776f5d5095be5893f2289
joss-c <joss.classey@gmail.com>
2018-10-21 20:37:17 +0400

    Updates

---
+++ precache-manifest.45744bd7429863c90339d43fb46c375d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "10489d8875d800e3a0a5",
+    "url": "/react-todo-v2/static/js/runtime~main.10489d88.js"
+  },
+  {
+    "revision": "6814eb75563f0b0d031c",
+    "url": "/react-todo-v2/static/js/main.6814eb75.chunk.js"
+  },
+  {
+    "revision": "941d05c2dbe52a5b7aa6",
+    "url": "/react-todo-v2/static/js/1.941d05c2.chunk.js"
+  },
+  {
+    "revision": "6814eb75563f0b0d031c",
+    "url": "/react-todo-v2/static/css/main.8b2ac032.chunk.css"
+  },
+  {
+    "revision": "941d05c2dbe52a5b7aa6",
+    "url": "/react-todo-v2/static/css/1.c408ad7e.chunk.css"
+  },
+  {
+    "revision": "bc80c751224766002cd60f5350e639a9",
+    "url": "/react-todo-v2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-todo-v2/precache-manifest.c95161573d2a7b0e9cf7640e5ea8f73d.js"
+  "/react-todo-v2/precache-manifest.45744bd7429863c90339d43fb46c375d.js"
 );
 
 workbox.clientsClaim();
