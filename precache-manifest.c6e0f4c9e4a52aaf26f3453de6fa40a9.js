266d5ccd4c7824291711dd7a38a0965e39665e90
joss-c <joss.classey@gmail.com>
2018-11-16 14:40:20 +0400

    Updates

--- precache-manifest.c6e0f4c9e4a52aaf26f3453de6fa40a9.js
+++ precache-manifest.c6e0f4c9e4a52aaf26f3453de6fa40a9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-todo-v2/static/js/runtime~main.10489d88.js"
   },
   {
-    "revision": "3c0c140a415cd8155572",
-    "url": "/react-todo-v2/static/js/main.3c0c140a.chunk.js"
+    "revision": "1c367a8662d90feaba7f",
+    "url": "/react-todo-v2/static/js/main.1c367a86.chunk.js"
   },
   {
     "revision": "33f6347182935a2baf07",
     "url": "/react-todo-v2/static/js/1.33f63471.chunk.js"
   },
   {
-    "revision": "3c0c140a415cd8155572",
+    "revision": "1c367a8662d90feaba7f",
     "url": "/react-todo-v2/static/css/main.95ba2add.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-todo-v2/static/css/1.dca95793.chunk.css"
   },
   {
-    "revision": "5015eac10e9f4617715d740352f9690a",
+    "revision": "6c3a864c167cd6a54090b1ef29585964",
     "url": "/react-todo-v2/index.html"
   }
 ];
