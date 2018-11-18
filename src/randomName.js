b765be790534e50173258e276f53dde6513d9c63
joss-c <joss.classey@gmail.com>
2018-11-18 14:37:36 +0400

    chore(randomName): Refactor + more intricate names

--- src/randomName.js
+++ src/randomName.js
@@ -1,17 +1,16 @@
+
 const randomName = () => {
     const consonants = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
     const doubleConsonants = ['bb', 'dd', 'ff', 'gg', 'll', 'mm', 'nn', 'pp', 'rr', 'ss', 'tt', 'vv', 'zz']
     const vowels = ['a', 'e', 'i', 'o', 'u']
-    const endings = ['y', 'a', 's', 'ie', 'er', 'el', 'on', 'en', 'es', 'sy', 'ely', 'ley', 'ina', 'ia', 'ers', 'elyn', 'ica', 'arin', ]
-    const randomConsonant = () => consonants[Math.floor(Math.random() * consonants.length)]
-    const randomDoubleConsonant = () => doubleConsonants[Math.floor(Math.random() * doubleConsonants.length)]
-    const randomVowel = () => vowels[Math.floor(Math.random() * vowels.length)]
-    const randomEnding = () => endings[Math.floor(Math.random() * endings.length)]
+    const doubleVowels = ['au', 'ai', 'ee', 'eo', 'ia', 'io', 'oo', 'ou']
+    const endings = ['y', 'a', 'e', 's', 'ie', 'ia', 'er', 'el', 'on', 'en', 'ur', 'es', 'as', 'sy', 'ely', 'ley', 'ina', 'ers', 'elyn', 'ica', 'arin', ]
+    const randomLetter = (type) => type[Math.floor(Math.random() * type.length)]
     let newName =
-        randomConsonant() + 
-        randomVowel() + 
-        ((Math.floor(Math.random() * 10) + 1 > 7) ? randomDoubleConsonant() : randomConsonant()) + 
-        randomEnding()
+        randomLetter(consonants) + 
+        ((Math.random() > 0.7) ? randomLetter(doubleVowels) : randomLetter(vowels)) + 
+        ((Math.random() > 0.7) ? randomLetter(doubleConsonants) : randomLetter(consonants)) + 
+        randomLetter(endings)
     newName = newName.charAt(0).toUpperCase() + newName.slice(1)
     return newName
 }
